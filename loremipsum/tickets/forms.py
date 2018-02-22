7ea9426265e01c3f49e94f314e82e8825120a0b5
canagno123 <xristosanagnostou95@hotmail.com>
2018-02-22 19:00:31 +0200

    Greek support and Provider Profile

--- loremipsum/tickets/forms.py
+++ loremipsum/tickets/forms.py
@@ -7,9 +7,9 @@ from tickets.validators import *
 
 class ParentSignUpForm(UserCreationForm):
 
-	first_name = forms.CharField(max_length=30, label='First Name')
-	last_name = forms.CharField(max_length=30, label='Last Name')
-	address = forms.CharField(max_length=30, label='Address')
+	first_name = forms.CharField(max_length=30, label='Όνομα')
+	last_name = forms.CharField(max_length=30, label='Επώνυμο')
+	address = forms.CharField(max_length=30, label='Διεύθυνση')
 	email = MyEmailField(max_length=30, help_text='example@example.com', label='Email')
 
 	class Meta(UserCreationForm.Meta):
@@ -32,15 +32,15 @@ class ParentSignUpForm(UserCreationForm):
 
 class ProviderSignUpForm(UserCreationForm):
 
-	first_name = forms.CharField(max_length=30, label='First Name')
-	last_name = forms.CharField(max_length=30, label='Last Name')
+	first_name = forms.CharField(max_length=30, label='Όνομα')
+	last_name = forms.CharField(max_length=30, label='Επώνυμο')
 	address = forms.CharField(max_length=30, label='Διεύθυνση')
 	email = MyEmailField(max_length=30, help_text='example@example.com', label='Email')
 	afm = forms.CharField(max_length=30, help_text= 'π.χ. 123456789', label='ΑΦΜ', validators=[RegexValidator(regex="^\d{9}$", message="Παρακαλώ εισάγετε έγκυρο ΑΦΜ (9 ψηφία)")])
 	doy = forms.CharField(max_length=30, label='ΔΟΥ')
 	lr = forms.CharField(max_length=30, label='Νομικός Εκπρόσωπος')
 	adt = forms.CharField(max_length=30, label='Αριθμός Δελτίου Ταυτότητας', validators=[RegexValidator(regex="^\d{6,7}$", message="Παρακαλώ εισάγετε έγκυρο ΑΔΤ (6-7 ψηφία)")])
-	site = forms.URLField(max_length=30, required=False, label='WebSite', validators=[URLValidator(message="Παρακαλώ εισάγετε έγκυρη URL")])
+	site = forms.URLField(max_length=30, required=False, label='Ιστοσελίδα', validators=[URLValidator(message="Παρακαλώ εισάγετε έγκυρη URL")])
 
 
 	class Meta(UserCreationForm.Meta):
@@ -56,15 +56,15 @@ class ProviderSignUpForm(UserCreationForm):
 		user.is_provider = True
 		user.save()
 		_site = self.cleaned_data['site']
-		parent = Provider.objects.create(user=user, full_name=fn, address=self.cleaned_data['address'], email= user.email,
+		provider = Provider.objects.create(user=user, full_name=fn, address=self.cleaned_data['address'], email= user.email,
 			afm=self.cleaned_data['afm'], doy=self.cleaned_data['doy'], legal_representative=self.cleaned_data['lr'], adt=self.cleaned_data['adt'], site=(None if _site == None else _site))
 		return user
 
-
+"""
 class SignupForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='Full Name')
     last_name = forms.CharField(max_length=30, label='Last Name')
-    address = forms.CharField(max_length=30, label='Last Name')
+    address = forms.CharField(max_length=30, label='Διεύθυνση')
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
@@ -74,3 +74,4 @@ class SignupForm(forms.Form):
        	#up.full_name = self.cleaned_data['full_name']
        	user.save()
        	up.save()
+"""
